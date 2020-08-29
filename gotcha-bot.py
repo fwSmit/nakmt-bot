85ac91bff566b48b7f8944c456c3d1c7e7d197c2
fwsmit <fw.smit01@gmail.com>
2020-08-29 18:38:54 +0200

    started converting to datetime

--- gotcha-bot.py
+++ gotcha-bot.py
@@ -6,7 +6,7 @@ import discord
 from dotenv import load_dotenv
 import time
 import math
-import datetime as dt
+from datetime import datetime, timedelta
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
@@ -63,9 +63,10 @@ class MyClient(discord.Client):
             if member.id not in self.names_by_id:
                 # I dont know this person, adding to the dictionary
                 self.names_by_id[member.id] = member.display_name
-                self.totalTimes[member.id] = 0
-                self.totalTimesNoTimeSlot[member.id] = 0
-            self.timeJoined[member.id] = time.time()
+                self.totalTimes[member.id] = timedelta(0)
+                self.totalTimesNoTimeSlot[member.id] = timedelta(0)
+
+            self.timeJoined[member.id] = datetime.now()
                 
         if before.channel != None and after.channel == None:
             print("Left voice channel")
@@ -73,20 +74,15 @@ class MyClient(discord.Client):
                 print("ERROR: don't know this person leaving the voice chat")
                 return
 
-            joined_s = self.timeJoined[member.id]
-            # _l means localtime and _s means seconds since epoch
-            endTime_l = time.localtime()
-            beginTime_s = joined_s
-            beginTime_l = time.localtime(beginTime_s)
-            endTime_s = time.mktime(endTime_l)
-            diff_s = endTime_s - beginTime_s
-
-            self.totalTimesNoTimeSlot[member.id] += diff_s
+            beginTime = self.timeJoined[member.id]
+            endTime = datetime.now()
+            diffTime = endTime-beginTime
+            self.totalTimesNoTimeSlot[member.id] += diffTime
             
             # I'm assuming total time is not more than like 24 hours, so the duration doesn't span accross an entire timespan
             
             # both in time slot
-            if self.isInTimeSlot(beginTime_l) and self.isInTimeSlot(endTime_l):
+            if self.isInTimeSlot(beginTime) and self.isInTimeSlot(endTime):
                 self.totalTimes[member.id] += diff_s
                 print("Duration (normal case): {} seconds".format(diff_s))
                 
@@ -125,11 +121,12 @@ class MyClient(discord.Client):
         # member.id
         # member.display_name
 
-    def isInTimeSlot(self, t):
+    def isInTimeSlot(self, t: datetime):
         # Time slot is Mon-Fri 16:00 - 04:00
-        if t.tm_hour >= 16 and t.tm_wday <= 4:
+        weekday = t.date().weekday()
+        if t.hour >= 16 and weekday <= 4:
             return True
-        if t.tm_hour <= 4 and t.tm_wday > 0 and t.tm_wday <= 5:
+        if t.hour <= 4 and weekday > 0 and weekday <= 5:
             return True
 
 
