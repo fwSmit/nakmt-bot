e3a339d91449d872294d7d039ea39846801837f6
fwsmit <fw.smit01@gmail.com>
2020-09-07 22:51:58 +0200

    chatbot

--- chatBot.py
+++ chatBot.py
@@ -1,12 +1,11 @@
 #!/bin/python
-
 # bot.py
 import os
 import discord
 from dotenv import load_dotenv
 
 load_dotenv()
-TOKEN = os.getenv('DISCORD_TOKEN')
+TOKEN = os.getenv('DISCORD_TOKEN2')
 
 GAME = False
 
@@ -24,12 +23,12 @@ class MyClient(discord.Client):
             print("recieved PM")
             print(message.content)
             print(message.author)
-            await message.channel.send("Recieved your message. Starting conversation")
-            if message.author not in self.conversations:
+            if message.author.id not in self.conversations:
                 print("I don't know this person. Making new conversation")
-                self.conversations[message.author] = Conversation()
-
-            await self.conversations[message.author].talk(message)
+                self.conversations[message.author.id] = Conversation()
+                await self.conversations[message.author.id].start(message.channel)
+            else:
+                await self.conversations[message.author.id].talk(message)
             
 
     async def on_ready(self):
@@ -37,13 +36,17 @@ class MyClient(discord.Client):
 
 
 class Conversation:
+    testing = False
     progress = 0
     questions = [
             "First question: What is my name?",
-            "Well done."
+            #  "Second question",
+            "Third question"
     ]
     answers = [
-            "Bob"
+            "Bob",
+            #  "Second",
+            "Third"
     ]
 
     def validateAnswer(self, content):
@@ -54,14 +57,33 @@ class Conversation:
         
         return False
     
+    async def say(self, string, channel):
+        if self.testing:
+            print(string)
+        else:
+            await channel.send(string)
+    
+    async def start(self, channel):
+        await self.say(self.questions[0], channel)
+
     async def talk(self, message):
-        #  await message.channel.send("Received message: {}".format(message.content))
-        question = self.questions[0]
-        await message.channel.send(question)
-        await message.channel.send("Progress: {}".format(self.progress))
+        # await message.channel.send("Received message: {}".format(message.content))
+        if self.progress > len(self.answers)-1:
+            await self.say("There are no more questions", message.channel)
+            return
+        
         if self.validateAnswer(message.content):
-            await message.channel.send("That's the right answer")
+            await self.say("That's the right answer", message.channel)
             self.progress = self.progress + 1
+            if self.progress > len(self.questions):
+                await self.say("You have gotten all questions correct!", message.channel)
+            else:
+                question = self.questions[self.progress]
+                await self.say(question, message.channel)
+                await self.say("Progress: {}".format(self.progress), message.channel)
+        else:
+            await self.say("That's not the right answer", message.channel)
+            await self.say("The right answer is {}".format(self.answers[self.progress]), message.channel)
         # receive message
 
 
